13cd213a7b143d0fd4d778ee5a3853264d26b2ce
Donlon <idonlon@hotmail.com>
2021-04-21 19:23:15 +0800

    eic-final: 添加BSP代码

---
+++ eic-final/bsp/standalone_bsp_0/CORTEXM3_AXI_0/include/xparameters.h
@@ -0,0 +1,96 @@
+#ifndef XPARAMETERS_H   /* prevent circular inclusions */
+#define XPARAMETERS_H   /* by using protection macros */
+
+/* Definition for CPU ID */
+#define XPAR_CPU_ID 0U
+
+/* Definitions for peripheral CORTEXM3_AXI_0 */
+#define XPAR_CORTEXM3_AXI_0_CPU_CLK_FREQ_HZ 0
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral CORTEXM3_AXI_0 */
+#define XPAR_CPU_CORTEXM3_0_CPU_CLK_FREQ_HZ 0
+
+
+/******************************************************************/
+
+
+/******************************************************************/
+
+/* Platform specific definitions */
+#define PLATFORM_ARM
+ 
+/* Definitions for sleep timer configuration */
+#define XSLEEP_TIMER_IS_DEFAULT_TIMER
+ 
+ 
+/******************************************************************/
+/* Definitions for driver BRAM */
+#define XPAR_XBRAM_NUM_INSTANCES 1U
+
+/* Definitions for peripheral AXI_BRAM_CTRL_0 */
+#define XPAR_AXI_BRAM_CTRL_0_DEVICE_ID 0U
+#define XPAR_AXI_BRAM_CTRL_0_DATA_WIDTH 32U
+#define XPAR_AXI_BRAM_CTRL_0_ECC 0U
+#define XPAR_AXI_BRAM_CTRL_0_FAULT_INJECT 0U
+#define XPAR_AXI_BRAM_CTRL_0_CE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_UE_FAILING_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_STATUS_REGISTERS 0U
+#define XPAR_AXI_BRAM_CTRL_0_CE_COUNTER_WIDTH 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_ONOFF_REGISTER 0U
+#define XPAR_AXI_BRAM_CTRL_0_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_AXI_BRAM_CTRL_0_WRITE_ACCESS 0U
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR 0x02000000U
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_HIGHADDR 0x02001FFFU
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_AXI_BRAM_CTRL_0_S_AXI_CTRL_HIGHADDR 0xFFFFFFFFU  
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral AXI_BRAM_CTRL_0 */
+#define XPAR_BRAM_0_DEVICE_ID XPAR_AXI_BRAM_CTRL_0_DEVICE_ID
+#define XPAR_BRAM_0_DATA_WIDTH 32U
+#define XPAR_BRAM_0_ECC 0U
+#define XPAR_BRAM_0_FAULT_INJECT 0U
+#define XPAR_BRAM_0_CE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_0_UE_FAILING_REGISTERS 0U
+#define XPAR_BRAM_0_ECC_STATUS_REGISTERS 0U
+#define XPAR_BRAM_0_CE_COUNTER_WIDTH 0U
+#define XPAR_BRAM_0_ECC_ONOFF_REGISTER 0U
+#define XPAR_BRAM_0_ECC_ONOFF_RESET_VALUE 0U
+#define XPAR_BRAM_0_WRITE_ACCESS 0U
+#define XPAR_BRAM_0_BASEADDR 0x02000000U
+#define XPAR_BRAM_0_HIGHADDR 0x02001FFFU
+#define XPAR_BRAM_0_CTRL_BASEADDR 0xFFFFFFFFU  
+#define XPAR_BRAM_0_CTRL_HIGHADDR 0xFFFFFFFEU  
+
+
+/******************************************************************/
+
+/* Definitions for driver GPIO */
+#define XPAR_XGPIO_NUM_INSTANCES 1
+
+/* Definitions for peripheral AXI_GPIO_0 */
+#define XPAR_AXI_GPIO_0_BASEADDR 0x40100000
+#define XPAR_AXI_GPIO_0_HIGHADDR 0x4010FFFF
+#define XPAR_AXI_GPIO_0_DEVICE_ID 0
+#define XPAR_AXI_GPIO_0_INTERRUPT_PRESENT 1
+#define XPAR_AXI_GPIO_0_IS_DUAL 0
+
+
+/******************************************************************/
+
+/* Canonical definitions for peripheral AXI_GPIO_0 */
+#define XPAR_GPIO_0_BASEADDR 0x40100000
+#define XPAR_GPIO_0_HIGHADDR 0x4010FFFF
+#define XPAR_GPIO_0_DEVICE_ID XPAR_AXI_GPIO_0_DEVICE_ID
+#define XPAR_GPIO_0_INTERRUPT_PRESENT 1
+#define XPAR_GPIO_0_IS_DUAL 0
+
+
+/******************************************************************/
+
+#endif  /* end of protection macro */
--- eic-final/software/main.c
+++ eic-final/software/main.c
@@ -2,12 +2,10 @@
 
 #include <xparameters.h>
 #include <xgpio.h>
-#include <xuartlite.h>
 
 int value = 1;
 
 XGpio gpio;   /* The driver instance for GPIO Device 0 */
-XUartLite uart_inst;         /* Instance of the UartLite device */
 
 int systick_init() {
     return SysTick_Config(50e6 / 4); // 4 Hz
@@ -24,31 +22,8 @@ int gpio_init() {
     return status;
 }
 
-int uart_init() {
-    /*
-     * Initialize the UartLite driver so that it's ready to use.
-     */
-    int status = XUartLite_Initialize(&uart_inst, XPAR_UARTLITE_0_DEVICE_ID);
-    if (status != XST_SUCCESS) {
-        return XST_FAILURE;
-    }
-
-    /*
-     * Perform a self-test to ensure that the hardware was built correctly.
-     */
-    status = XUartLite_SelfTest(&uart_inst);
-    if (status != XST_SUCCESS) {
-        return XST_FAILURE;
-    }
-    return status;
-}
-
 int system_init() {
     int status;
-    status = uart_init();
-    if (status != XST_SUCCESS) {
-        return status;
-    }
     status = systick_init();
     if (status != XST_SUCCESS) {
         return status;
@@ -65,9 +40,6 @@ int main() {
     if (status != XST_SUCCESS) {
         while (1);
     }
-    xil_printf("\r\n\r\n");
-    xil_printf("=== Cortex-M3 SoC designed by Team CICC2985 ===\r\n");
-    xil_printf("Hello World!\r\n");
     return 0;
 }
 
@@ -76,9 +48,6 @@ void SysTick_Handler() {
         XGpio_DiscreteWrite(&gpio, 1, 0b01);
     } else {
         XGpio_DiscreteWrite(&gpio, 1, 0b10);
-    }
-    if (value % 4 == 0) {
-        xil_printf("Hello from Cortex-M3 soft-core processor, t=%ds\r\n", value / 4);
     }
     value++;
 }
