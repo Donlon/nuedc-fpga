7fadde4b85eb8ca334625d7a134f36950bf91c0e
Donlon <idonlon@hotmail.com>
2021-05-02 17:22:55 +0800

    axidac-test: 更新测试代码

--- ip-tests/axidac-test/sim/tb_repeat_mode.sv
+++ ip-tests/axidac-test/sim/tb_repeat_mode.sv
@@ -61,6 +61,26 @@ module tb_repeat_mode();
         master_agent.AXI4LITE_WRITE_BURST(base_addr + offset, 0, data, resp);
     endtask : axi_dac_write_reg
 
+    task prepare_waveform (
+        input   axi_vip_1_axi_vip_s_0_slv_mem_t  slv,
+        input   bit [31:0]     start_addr,
+        input   bit [31:0]     length
+    );
+        real v_real;
+        bit [7:0] v;
+        for (int i = 0; i < length; i++) begin
+            v_real = ($sin(i * 2 * 3.1415926 / length) + 1) / 2 * 255;
+            v = $rtoi(v_real);
+            case (i[1:0])
+                2'b00: slv.mem_model.backdoor_memory_write(start_addr + i & ~(4 - 1), {24'b0, v},       4'b0001);
+                2'b01: slv.mem_model.backdoor_memory_write(start_addr + i & ~(4 - 1), {16'b0, v, 8'b0}, 4'b0010);
+                2'b10: slv.mem_model.backdoor_memory_write(start_addr + i & ~(4 - 1), {8'b0, v, 16'b0}, 4'b0100);
+                2'b11: slv.mem_model.backdoor_memory_write(start_addr + i & ~(4 - 1), {v, 24'b0},       4'b1000);
+            endcase
+        end
+
+    endtask
+
     //////////////////////////////////////////////////////////////////////////////////
     // The following part controls the AXI VIP.
     // It follows the "Usefull Coding Guidelines and Examples" section from PG267
@@ -97,6 +117,7 @@ module tb_repeat_mode();
 
         /// ===================== size=256, div=5, repeat =====================
         #50ns
+        prepare_waveform(slave_agent, 32'h20000000, 16'd256);
         axi_dac_write_reg(REG_SRC_ADDR, 32'h20000000, resp);
         axi_dac_write_reg(REG_SRC_SIZE, 16'd256, resp); // 4 * brust16
         axi_dac_write_reg(REG_CLK_DIV, 5, resp); // Clock div: 5
@@ -108,6 +129,7 @@ module tb_repeat_mode();
         #1us $stop;
 
         /// ===================== size=128, div=7, loop =====================
+        prepare_waveform(slave_agent, 32'h20000000, 16'd128);
         axi_dac_write_reg(REG_SRC_SIZE, 16'd128, resp); // 2 * brust16
         axi_dac_write_reg(REG_CLK_DIV, 7, resp); // Clock div: 7
         axi_dac_read_reg(REG_CTL, data, resp);
@@ -123,19 +145,34 @@ module tb_repeat_mode();
         $display("[%t] Loop mode test ended.", $realtime);
         #1us $stop;
 
-        /// ===================== size=84, div=5, repeat =====================
+        /// ===================== size=100, div=3, repeat =====================
         #50ns
+        prepare_waveform(slave_agent, 32'h20000000, 16'd100);
         axi_dac_write_reg(REG_SRC_SIZE, 16'd100, resp); // 1 * brust16 + 9*4
         axi_dac_write_reg(REG_CLK_DIV, 3, resp); // Clock div: 5
         axi_dac_read_reg(REG_CTL, data, resp);
-        axi_dac_write_reg(REG_CTL, data | (1 << CTL_REG_REPEAT_BIT) | (1 << CTL_REG_START_BIT), resp);
+        axi_dac_write_reg(REG_CTL, data & ~(1 << CTL_REG_LOOP_BIT) | (1 << CTL_REG_REPEAT_BIT) | (1 << CTL_REG_START_BIT), resp);
+
+        wait (~u_axi_vip.axi_vip_1_i.axi_dac_0.inst.axi_dac_v0_1_S_AXI_inst.busy);
+        $display("[%t] Repeat mode test ended.", $realtime);
+        #1us $stop;
+
+        /// ===================== size=140, div=2, loop =====================
+        prepare_waveform(slave_agent, 32'h20000000, 16'd140);
+        axi_dac_write_reg(REG_SRC_SIZE, 16'd140, resp); // 2 * brust16 + 3*4
+        axi_dac_write_reg(REG_CLK_DIV, 2, resp); // Clock div: 2
+        axi_dac_read_reg(REG_CTL, data, resp);
+        axi_dac_write_reg(REG_CTL,
+                          data | (1 << CTL_REG_LOOP_BIT) | (1 << CTL_REG_START_BIT),
+                          resp);
+
         #20us
         axi_dac_read_reg(REG_CTL, data, resp);
         axi_dac_write_reg(REG_CTL, data | (1 << CTL_REG_STOP_BIT), resp);
         $display("[%t] Stop signal was sent.", $realtime);
 
         wait (~u_axi_vip.axi_vip_1_i.axi_dac_0.inst.axi_dac_v0_1_S_AXI_inst.busy);
-        $display("[%t] Repeat mode test 2 ended.", $realtime);
+        $display("[%t] Loop mode test 2 ended.", $realtime);
         #1us $stop;
  
     end
