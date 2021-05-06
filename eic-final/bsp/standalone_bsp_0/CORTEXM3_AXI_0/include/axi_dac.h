aa643e816b1f69adf750f34561311447f35cdf12
Donlon <idonlon@hotmail.com>
2021-05-07 01:11:51 +0800

    update submodule ip_repo

--- eic-final/bsp/standalone_bsp_0/CORTEXM3_AXI_0/include/axi_dac.h
+++ eic-final/bsp/standalone_bsp_0/CORTEXM3_AXI_0/include/axi_dac.h
@@ -2,113 +2,31 @@
 #ifndef AXI_DAC_H
 #define AXI_DAC_H
 
-
 /****************** Include Files ********************/
-#include "xil_types.h"
 #include "xstatus.h"
 
-#define AXI_DAC_CTL_REG_OFFSET       0
-#define AXI_DAC_DATA_REG_OFFSET      4
-#define AXI_DAC_SRC_ADDR_REG_OFFSET  8
-#define AXI_DAC_SRC_SIZE_REG_OFFSET 12
-#define AXI_DAC_CLK_DIV_REG_OFFSET  16
+#define AXI_DAC_CTL_REG_OFFSET      0x00
+#define AXI_DAC_DATA_REG_OFFSET     0x04
+#define AXI_DAC_SRC_ADDR_REG_OFFSET 0x08
+#define AXI_DAC_SRC_SIZE_REG_OFFSET 0x0c
+#define AXI_DAC_CLK_DIV_REG_OFFSET  0x10
 
-#define AXI_DAC_CTL_REG_REPEAT_BIT  0x00
-#define AXI_DAC_CTL_REG_LOOP_BIT    0x01
-#define AXI_DAC_CTL_REG_BUSY_BIT    0x02
-#define AXI_DAC_CTL_REG_START_BIT   0x03
-#define AXI_DAC_CTL_REG_STOP_BIT    0x04
+#define AXI_DAC_CTL_REG_REPEAT_BIT  0x01
+#define AXI_DAC_CTL_REG_LOOP_BIT    0x02
+#define AXI_DAC_CTL_REG_BUSY_BIT    0x04
+#define AXI_DAC_CTL_REG_START_BIT   0x08
+#define AXI_DAC_CTL_REG_STOP_BIT    0x10
 
+XStatus AXI_DAC_convertSingle(uint32_t baseAddress, uint8_t data);
 
-/**************************** Type Definitions *****************************/
-/**
- *
- * Write a value to a AXI_DAC register. A 32 bit write is performed.
- * If the component is implemented in a smaller width, only the least
- * significant data is written.
- *
- * @param   BaseAddress is the base address of the AXI_DACdevice.
- * @param   RegOffset is the register offset from the base to write to.
- * @param   Data is the data written to the register.
- *
- * @return  None.
- *
- * @note
- * C-style signature:
- * 	void AXI_DAC_mWriteReg(u32 BaseAddress, unsigned RegOffset, u32 Data)
- *
- */
-#define AXI_DAC_mWriteReg(BaseAddress, RegOffset, Data) \
-  	Xil_Out32((BaseAddress) + (RegOffset), (u32)(Data))
+XStatus AXI_DAC_setOutputClockDivision(uint32_t baseAddress, uint16_t division);
 
-/**
- *
- * Read a value from a AXI_DAC register. A 32 bit read is performed.
- * If the component is implemented in a smaller width, only the least
- * significant data is read from the register. The most significant data
- * will be read as 0.
- *
- * @param   BaseAddress is the base address of the AXI_DAC device.
- * @param   RegOffset is the register offset from the base to write to.
- *
- * @return  Data is the data from the register.
- *
- * @note
- * C-style signature:
- * 	u32 AXI_DAC_mReadReg(u32 BaseAddress, unsigned RegOffset)
- *
- */
-#define AXI_DAC_mReadReg(BaseAddress, RegOffset) \
-    Xil_In32((BaseAddress) + (RegOffset))
+XStatus AXI_DAC_startSequenceConversion(uint32_t baseAddress, const uint8_t *data_src, u32 length);
 
-/************************** Function Prototypes ****************************/
-/**
- *
- * Run a self-test on the driver/device. Note this may be a destructive test if
- * resets of the device are performed.
- *
- * If the hardware system is not built correctly, this function may never
- * return to the caller.
- *
- * @param   baseaddr_p is the base address of the AXI_DAC instance to be worked on.
- *
- * @return
- *
- *    - XST_SUCCESS   if all self-test code passed
- *    - XST_FAILURE   if any self-test code failed
- *
- * @note    Caching must be turned off for this function to work.
- * @note    Self test may fail if data memory and device are not on the same bus.
- *
- */
-XStatus AXI_DAC_Reg_SelfTest(void * baseaddr_p);
+XStatus AXI_DAC_startSequenceConversionLooped(uint32_t baseAddress, const uint8_t *data_src, u32 length);
 
-/**
- *
- * Run a self-test on the driver/device. Note this may be a destructive test if
- * resets of the device are performed.
- *
- * If the hardware system is not built correctly, this function may never
- * return to the caller.
- *
- * @param   baseaddr_p is the base address of the AXI_DAC instance to be worked on.
- * @param   data       DAC output data
- */
-XStatus AXI_DAC_convertSingle(void *baseaddr_p, u8 data);
+XStatus AXI_DAC_isBusy(uint32_t baseAddress, uint8_t *busy);
 
-// not implemented
-XStatus AXI_DAC_setDacClockDivision(void *baseaddr_p, u16 division);
-
-// not implemented
-XStatus AXI_DAC_startSequenceConversion(void *baseaddr_p, u8 *data_src, u32 length);
-
-// not implemented
-XStatus AXI_DAC_startSequenceConversionLooped(void *baseaddr_p, u8 *data_src, u32 length);
-
-// not implemented
-XStatus AXI_DAC_isBusy(void *baseaddr_p, u8 *busy);
-
-// not implemented
-XStatus AXI_DAC_stopConversion(void *baseaddr_p);
+XStatus AXI_DAC_stopConversion(uint32_t baseAddress);
 
 #endif // AXI_DAC_H
--- eic-final/bsp/standalone_bsp_0/CORTEXM3_AXI_0/libsrc/axi_dac_v1_0/src/axi_dac.c
+++ eic-final/bsp/standalone_bsp_0/CORTEXM3_AXI_0/libsrc/axi_dac_v1_0/src/axi_dac.c
@@ -2,53 +2,45 @@
 
 /***************************** Include Files *******************************/
 #include "axi_dac.h"
-#include "xil_io.h"
 
-#define REG32(base, offset) (*(volatile u32 *) ((u32) (base) + (offset)))
+#define REG32(base, offset) (*(volatile uint32_t *) ((u32) (base) + (offset)))
 
 /************************** Function Definitions ***************************/
-XStatus AXI_DAC_convertSingle(void *baseaddr_p, u8 data) {
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) &= ~(1 << AXI_DAC_CTL_REG_REPEAT_BIT);
-
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_DATA_REG_OFFSET, (u32) data);
-
+XStatus AXI_DAC_convertSingle(uint32_t baseAddress, u8 data) {
+    REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) &= ~AXI_DAC_CTL_REG_REPEAT_BIT;
+    REG32(baseAddress, AXI_DAC_DATA_REG_OFFSET) = data;
     return XST_SUCCESS;
 }
 
-XStatus AXI_DAC_setDacClockDivision(void *baseaddr_p, u16 division) {
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_CLK_DIV_REG_OFFSET, (u32) division);
-
+XStatus AXI_DAC_setOutputClockDivision(uint32_t baseAddress, u16 division) {
+    REG32(baseAddress, AXI_DAC_CLK_DIV_REG_OFFSET) = division;
     return XST_SUCCESS;
 }
 
-XStatus AXI_DAC_startSequenceConversion(void *baseaddr_p, u8 *data_src, u32 length) {
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_SRC_ADDR_REG_OFFSET, (u32) data_src);
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_SRC_SIZE_REG_OFFSET, (u32) length);
+XStatus AXI_DAC_startSequenceConversion(uint32_t baseAddress, const u8 *data_src, u32 length) {
+    REG32(baseAddress, AXI_DAC_SRC_ADDR_REG_OFFSET) = (uint32_t) data_src;
+    REG32(baseAddress, AXI_DAC_SRC_SIZE_REG_OFFSET) = length;
 
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_REPEAT_BIT);
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) &= ~(1 << AXI_DAC_CTL_REG_LOOP_BIT);
-
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_START_BIT);
+    REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) &= ~AXI_DAC_CTL_REG_LOOP_BIT;
+    REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) |= AXI_DAC_CTL_REG_REPEAT_BIT + AXI_DAC_CTL_REG_START_BIT;
     return XST_SUCCESS;
 }
 
-XStatus AXI_DAC_startSequenceConversionLooped(void *baseaddr_p, u8 *data_src, u32 length) {
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_SRC_ADDR_REG_OFFSET, (u32) data_src);
-    AXI_DAC_mWriteReg((u32) baseaddr_p, AXI_DAC_SRC_SIZE_REG_OFFSET, (u32) length);
+XStatus AXI_DAC_startSequenceConversionLooped(uint32_t baseAddress, const u8 *data_src, u32 length) {
+    REG32(baseAddress, AXI_DAC_SRC_ADDR_REG_OFFSET) = (uint32_t) data_src;
+    REG32(baseAddress, AXI_DAC_SRC_SIZE_REG_OFFSET) = length;
 
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_REPEAT_BIT);
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_LOOP_BIT);
-
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_START_BIT);
+    REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) |= AXI_DAC_CTL_REG_REPEAT_BIT + AXI_DAC_CTL_REG_LOOP_BIT
+                                                  + AXI_DAC_CTL_REG_START_BIT;
     return XST_SUCCESS;
 }
 
-XStatus AXI_DAC_isBusy(void *baseaddr_p, u8 *busy) {
-	*busy = (AXI_DAC_mReadReg((u32) baseaddr_p, AXI_DAC_CTL_REG_OFFSET) & (1 << AXI_DAC_CTL_REG_BUSY_BIT)) ? 1 : 0;
+XStatus AXI_DAC_isBusy(uint32_t baseAddress, u8 *busy) {
+    *busy = (REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) & AXI_DAC_CTL_REG_BUSY_BIT) ? 1 : 0;
     return XST_SUCCESS;
 }
 
-XStatus AXI_DAC_stopConversion(void *baseaddr_p) {
-	REG32(baseaddr_p, AXI_DAC_CTL_REG_OFFSET) |= (1 << AXI_DAC_CTL_REG_STOP_BIT);
+XStatus AXI_DAC_stopConversion(uint32_t baseAddress) {
+    REG32(baseAddress, AXI_DAC_CTL_REG_OFFSET) |= AXI_DAC_CTL_REG_STOP_BIT;
     return XST_SUCCESS;
 }
